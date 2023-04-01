3f21b3c76440cdbecbb301ba7ef7320edd6a17f2
tuagkcuf <jokersms222@gmail.com>
2023-04-01 12:26:14 +0300

    add header component

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,11 @@ import ManualHeader from "../components/ManualHeader.jsx"
 
 export default function Home() {
     return (
-        <>
+        <>  
             <Head>
                 <title>Smart Contract Lottery</title>
                 <meta name="description" content="Our smart contract lottery" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
             </Head>
             <ManualHeader />
         </>
